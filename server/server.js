773cf4d57f45dd160289cbb8f1e14b940fd603c9
mengh <396714629@qq.com>
2018-05-23 14:51:40 +0800

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,8 @@
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send()
+})
+
+app.listen(5000)
---
+++ src/main.js
@@ -0,0 +1,28 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+// import App from './App'
+import Layout from './components/layout'
+import VueRouter from 'vue-router'
+import IndexPage from './pages/index'
+import VueResource from 'vue-resource'
+Vue.use(VueRouter)
+Vue.use(VueResource)
+let router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      component: IndexPage
+    }
+  ]
+})
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { Layout },
+  template: '<Layout/>'
+})
